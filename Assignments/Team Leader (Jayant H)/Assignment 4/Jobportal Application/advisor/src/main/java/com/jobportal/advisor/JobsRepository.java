641671a53a7ad51764db2d40d868e526a795517e
Jayant980 <58617050+Jayant980@users.noreply.github.com>
2022-10-23 09:23:13 +0530

    Delete Assignments/Team Leader (Jayant H)/Assignment 4/Jobportal Application/advisor directory

--- Assignments/Team Leader (Jayant H)/Assignment 4/Jobportal Application/advisor/src/main/java/com/jobportal/advisor/JobsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobportal.advisor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface JobsRepository extends JpaRepository<JobEntity, Long> {
-    public List<JobEntity> findAllByLocation(String location);
-}
