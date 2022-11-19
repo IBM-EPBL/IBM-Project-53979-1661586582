2037c7a14aaa230b4caa67750f69b8987718d79d
venkatasrinivaspv160602 <113846507+venkatasrinivaspv160602@users.noreply.github.com>
2022-11-19 21:14:29 +0530

    Delete Assignments/Team Member 4 (Venkatasrinivas P V)/Assignment 4/Jobportal Application directory

--- Assignments/Team Member 4 (Venkatasrinivas P V)/Assignment 4/Jobportal Application/search/src/main/java/com/jobportal/search/JobsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.jobportal.search;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobsRepository extends JpaRepository<JobEntity, Long>, JobsRepositoryCustom {
-}
--- Assignments/Team Member 4 (Venkatasrinivas P V)/Assignment 4/Jobportal Application/search/src/main/java/com/jobportal/search/JobsRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.jobportal.search;
-
-import java.util.List;
-
-public interface JobsRepositoryCustom {
-    public List<JobEntity> findJobByQuery(String q, String location);
-}
