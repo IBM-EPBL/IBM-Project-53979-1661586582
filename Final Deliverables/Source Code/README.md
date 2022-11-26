fe32481c2a29aa772e01ffe7396c83cc87b4c861
Jayant980 <58617050+Jayant980@users.noreply.github.com>
2022-11-26 16:37:50 +0530

    Delete Final Deliverables/Source Code directory

--- Final Deliverables/Source Code/README.md
+++
@@ -1,6 +0,0 @@
-
-# Project Title
-News Tracker Application
-
-# Team ID
-    PNT2022TMID21106
--- Final Deliverables/Source Code/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .config import App
-
-app = Flask(__name__)
-app.secret_key = App.SECRET_KEY
-
-
-from app import views
